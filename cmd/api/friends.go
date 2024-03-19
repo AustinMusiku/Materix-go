6cb496afc5cd2b02bfd54ffb821f07ec295ad293
Austin Musiku <musiku47@gmail.com>
2024-03-20 01:14:43 +0300

    refactor:  refactor error handling in friend resource endpoints
    Adopt generic error response functions in friend endpoint handlers

--- cmd/api/friends.go
+++ cmd/api/friends.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"net/http"
 	"strconv"
 
@@ -13,23 +14,26 @@ import (
 func (app *application) getMyFriendsHandler(w http.ResponseWriter, r *http.Request) {
 	u, ok := r.Context().Value(userContextKey).(*data.User)
 	if !ok {
-		app.writeJSON(w, http.StatusInternalServerError, ResponseWrapper{"error": "Internal server error"}, nil)
+		app.serverErrorResponse(w, r, errors.New("context missing user value"))
 		return
 	}
 
 	friends, err := app.models.Friends.GetFriendsFor(u.Id)
 	if err != nil {
-		app.writeJSON(w, http.StatusInternalServerError, ResponseWrapper{"error": "Internal server error"}, nil)
+		app.serverErrorResponse(w, r, err)
 		return
 	}
 
-	app.writeJSON(w, http.StatusOK, ResponseWrapper{"friends": friends}, nil)
+	err = app.writeJSON(w, http.StatusOK, ResponseWrapper{"friends": friends}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
 }
 
 func (app *application) sendFriendRequestHandler(w http.ResponseWriter, r *http.Request) {
 	u, ok := r.Context().Value(userContextKey).(*data.User)
 	if !ok {
-		app.writeJSON(w, http.StatusInternalServerError, ResponseWrapper{"error": "Internal server error"}, nil)
+		app.serverErrorResponse(w, r, errors.New("context missing user value"))
 		return
 	}
 
@@ -39,7 +43,7 @@ func (app *application) sendFriendRequestHandler(w http.ResponseWriter, r *http.
 
 	err := json.NewDecoder(r.Body).Decode(&input)
 	if err != nil {
-		app.writeJSON(w, http.StatusInternalServerError, ResponseWrapper{"error": "Internal server error"}, nil)
+		app.badRequestResponse(w, r, err)
 		return
 	}
 
@@ -51,8 +55,7 @@ func (app *application) sendFriendRequestHandler(w http.ResponseWriter, r *http.
 
 	v := validator.New()
 	if data.ValidateFriendPair(v, fRequest); !v.Valid() {
-		errors, _ := json.MarshalIndent(v.Errors, "", "\t")
-		app.writeJSON(w, http.StatusUnprocessableEntity, ResponseWrapper{"error": "Invalid friend request", "errors": errors}, nil)
+		app.failedValidationResponse(w, r, v.Errors)
 		return
 	}
 
@@ -60,20 +63,30 @@ func (app *application) sendFriendRequestHandler(w http.ResponseWriter, r *http.
 	if err != nil {
 		switch err {
 		case data.ErrDuplicateFriendRequest:
-			app.writeJSON(w, http.StatusConflict, ResponseWrapper{"error": "Friend request already sent"}, nil)
+			v.AddError("id", "friend request between users already exists")
+			app.failedValidationResponse(w, r, v.Errors)
 		default:
-			app.writeJSON(w, http.StatusInternalServerError, ResponseWrapper{"error": "Internal server error"}, nil)
+			app.serverErrorResponse(w, r, err)
 		}
 		return
 	}
 
-	app.writeJSON(w, http.StatusCreated, ResponseWrapper{"message": "Friend request sent"}, nil)
+	err = app.writeJSON(w, http.StatusCreated, ResponseWrapper{"message": "Friend request sent"}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
 }
 
 func (app *application) acceptFriendRequestHandler(w http.ResponseWriter, r *http.Request) {
-	fRequestId, err := strconv.Atoi(chi.URLParam(r, "id"))
+	id := chi.URLParam(r, "id")
+	if id == "" {
+		app.badRequestResponse(w, r, errors.New("missing or invalid friend request id"))
+		return
+	}
+
+	fRequestId, err := strconv.Atoi(id)
 	if err != nil {
-		app.writeJSON(w, http.StatusInternalServerError, ResponseWrapper{"error": "Internal server error"}, nil)
+		app.serverErrorResponse(w, r, err)
 		return
 	}
 
@@ -81,32 +94,41 @@ func (app *application) acceptFriendRequestHandler(w http.ResponseWriter, r *htt
 	if err != nil {
 		switch err {
 		case data.ErrRecordNotFound:
-			app.writeJSON(w, http.StatusNotFound, ResponseWrapper{"error": "Friend request not found"}, nil)
+			app.notFoundResponse(w, r)
 		default:
-			app.writeJSON(w, http.StatusInternalServerError, ResponseWrapper{"error": "Internal server error"}, nil)
+			app.serverErrorResponse(w, r, err)
 		}
 		return
 	}
 
 	err = app.models.Friends.Accept(fRequest)
 	if err != nil {
-		app.writeJSON(w, http.StatusInternalServerError, ResponseWrapper{"error": "Internal server error"}, nil)
+		app.serverErrorResponse(w, r, err)
 		return
 	}
 
-	app.writeJSON(w, http.StatusOK, ResponseWrapper{"message": "Friend request accepted"}, nil)
+	err = app.writeJSON(w, http.StatusOK, ResponseWrapper{"message": "Friend request accepted"}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
 }
 
 func (app *application) rejectFriendRequestHandler(w http.ResponseWriter, r *http.Request) {
 	u, ok := r.Context().Value(userContextKey).(*data.User)
 	if !ok {
-		app.writeJSON(w, http.StatusInternalServerError, ResponseWrapper{"error": "Internal server error"}, nil)
+		app.serverErrorResponse(w, r, errors.New("context missing user value"))
 		return
 	}
 
-	fRequestId, err := strconv.Atoi(chi.URLParam(r, "id"))
+	id := chi.URLParam(r, "id")
+	if id == "" {
+		app.badRequestResponse(w, r, errors.New("missing or invalid friend request id"))
+		return
+	}
+
+	fRequestId, err := strconv.Atoi(id)
 	if err != nil {
-		app.writeJSON(w, http.StatusInternalServerError, ResponseWrapper{"error": "Internal server error"}, nil)
+		app.serverErrorResponse(w, r, err)
 		return
 	}
 
@@ -114,70 +136,85 @@ func (app *application) rejectFriendRequestHandler(w http.ResponseWriter, r *htt
 	if err != nil {
 		switch err {
 		case data.ErrRecordNotFound:
-			app.writeJSON(w, http.StatusNotFound, ResponseWrapper{"error": "Friend request not found"}, nil)
+			app.notFoundResponse(w, r)
 		default:
-			app.writeJSON(w, http.StatusInternalServerError, ResponseWrapper{"error": "Internal server error"}, nil)
+			app.serverErrorResponse(w, r, err)
 		}
 		return
 	}
 
 	// Ensure only the source or destination user can cancel/reject the request
 	if u.Id != fRequest.DestinationUserId || u.Id != fRequest.SourceUserId {
-		app.writeJSON(w, http.StatusForbidden, ResponseWrapper{"error": "Forbidden"}, nil)
+		app.notFoundResponse(w, r)
 		return
 	}
 
 	err = app.models.Friends.Delete(fRequest)
 	if err != nil {
-		app.writeJSON(w, http.StatusInternalServerError, ResponseWrapper{"error": "Internal server error"}, nil)
+		app.serverErrorResponse(w, r, err)
 		return
 	}
 
-	app.writeJSON(w, http.StatusOK, ResponseWrapper{"message": "Friend request rejected"}, nil)
+	err = app.writeJSON(w, http.StatusOK, ResponseWrapper{"message": "Friend request rejected"}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
 }
 
 func (app *application) getSentFriendRequestsHandler(w http.ResponseWriter, r *http.Request) {
 	u, ok := r.Context().Value(userContextKey).(*data.User)
 	if !ok {
-		app.writeJSON(w, http.StatusInternalServerError, ResponseWrapper{"error": "Internal server error"}, nil)
+		app.serverErrorResponse(w, r, errors.New("context missing user value"))
 		return
 	}
 
 	requests, err := app.models.Friends.GetSentFor(u.Id)
 	if err != nil {
-		app.writeJSON(w, http.StatusInternalServerError, ResponseWrapper{"error": "Internal server error"}, nil)
+		app.serverErrorResponse(w, r, err)
 		return
 	}
 
-	app.writeJSON(w, http.StatusOK, ResponseWrapper{"requests": requests}, nil)
+	err = app.writeJSON(w, http.StatusOK, ResponseWrapper{"requests": requests}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
 }
 
 func (app *application) getReceivedFriendRequestsHandler(w http.ResponseWriter, r *http.Request) {
 	u, ok := r.Context().Value(userContextKey).(*data.User)
 	if !ok {
-		app.writeJSON(w, http.StatusInternalServerError, ResponseWrapper{"error": "Internal server error"}, nil)
+		app.serverErrorResponse(w, r, errors.New("context missing user value"))
 		return
 	}
 
 	requests, err := app.models.Friends.GetReceivedFor(u.Id)
 	if err != nil {
-		app.writeJSON(w, http.StatusInternalServerError, ResponseWrapper{"error": "Internal server error"}, nil)
+		app.serverErrorResponse(w, r, err)
 		return
 	}
 
-	app.writeJSON(w, http.StatusOK, ResponseWrapper{"requests": requests}, nil)
+	err = app.writeJSON(w, http.StatusOK, ResponseWrapper{"requests": requests}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
 }
 
 func (app *application) removeFriendHandler(w http.ResponseWriter, r *http.Request) {
 	u, ok := r.Context().Value(userContextKey).(*data.User)
 	if !ok {
-		app.writeJSON(w, http.StatusInternalServerError, ResponseWrapper{"error": "Internal server error"}, nil)
+		app.serverErrorResponse(w, r, errors.New("context missing user value"))
 		return
 	}
 
-	fId, err := strconv.Atoi(chi.URLParam(r, "id"))
+	id := chi.URLParam(r, "id")
+	if id == "" {
+		app.badRequestResponse(w, r, errors.New("missing or invalid friend id"))
+		return
+	}
+
+	fId, err := strconv.Atoi(id)
 	if err != nil {
-		app.writeJSON(w, http.StatusInternalServerError, ResponseWrapper{"error": "Internal server error"}, nil)
+		app.serverErrorResponse(w, r, err)
 		return
 	}
 
@@ -185,9 +222,9 @@ func (app *application) removeFriendHandler(w http.ResponseWriter, r *http.Reque
 	if err != nil {
 		switch err {
 		case data.ErrRecordNotFound:
-			app.writeJSON(w, http.StatusNotFound, ResponseWrapper{"error": "Friend not found"}, nil)
+			app.notFoundResponse(w, r)
 		default:
-			app.writeJSON(w, http.StatusInternalServerError, ResponseWrapper{"error": "Internal server error"}, nil)
+			app.serverErrorResponse(w, r, err)
 		}
 		return
 	}
@@ -196,12 +233,15 @@ func (app *application) removeFriendHandler(w http.ResponseWriter, r *http.Reque
 	if err != nil {
 		switch err {
 		case data.ErrEditConflict:
-			app.writeJSON(w, http.StatusConflict, ResponseWrapper{"error": "Friend not found"}, nil)
+			app.notFoundResponse(w, r)
 		default:
-			app.writeJSON(w, http.StatusInternalServerError, ResponseWrapper{"error": "Internal server error"}, nil)
+			app.serverErrorResponse(w, r, err)
 		}
 		return
 	}
 
-	app.writeJSON(w, http.StatusOK, ResponseWrapper{"message": "Friend removed"}, nil)
+	err = app.writeJSON(w, http.StatusOK, ResponseWrapper{"message": "Friend removed"}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
 }
